b66f55a86d9a65af826db741624b5c80f0c901ae
Franklin Pimentel <franklin.pimentel.1998@gmail.com>
2020-04-23 20:38:03 -0500

    Se esta agregando la estructura basica html de los contacto de parte del cliente

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,7 +2,34 @@ import React from 'react';
 
 const Contact = (props) => {
     return (
-        <h1>Contacto</h1>
+        <section  className='content'>
+            <div className="izq-alt">
+                <h2>Contacto</h2>
+            </div>
+            <div className="der-alt">
+                <h1>Contacto</h1>
+                <form>
+                    
+                    <div className="form-group">
+                        <input type="text" className="form-control" name="name" 
+                        placeholder="Nombre"/>
+                    </div>
+
+                    <div className="form-group">
+                        <input type="email" className="form-control" name="email" 
+                        placeholder="Correo Electronico"/>
+                    </div>
+
+                    <div className="form-group">
+                        <textarea type="text" className="form-control" name="message" 
+                        placeholder="Mensaje"/>
+                    </div>
+
+                    <input type="submit" className="btn btn-primary" value="Enviar"/>
+
+                </form>
+            </div>
+        </section>
     );
 } 
 
