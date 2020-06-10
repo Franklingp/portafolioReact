d9df01253d400cc4a63c18ca4dcce2a1e042c1f2
Franklin Pimentel <franklin.pimentel.1998@gmail.com>
2020-06-09 19:55:53 -0500

    Cambiando el texto de sobre mi

--- src/pages/About.js
+++ src/pages/About.js
@@ -54,14 +54,16 @@ export const About = () => {
         <section className="about-der text-regular">
             <h3 className="about-me">Sobre mi</h3>
             <p className="paragraph text-ligth">
-                Hola red, yo soy Franklin Pimentel y soy Desarrollador web FullStack.
+                Franklin Pimentel Desarrollador Junior FullStack.
                 <br/>
-                Como FullStack Junior cuento conocimientos en Frond-end con React JS y Angular, 
-                junto a un buen manejo JavaScript como lenguaje de programación principal. Tambien manejo Backend con NodeJS
-                y Express para las bases de datos con MongoDB.
                 <br/>
-                La pagina que estas viendo actualmente esta construida con las tecnologias ya mencionadas
-                desplegadas en Firebase el Frond-end y el Backend en Heroku
+                Como FullStack cuento con conocimientos en Frond-end con React JS y Angular y en 
+                Backend manejo NodeJS junto con Express y por ultimo para la base de datos uso MongoDB.
+                <br/>
+                <br/>
+                La pagina que estas viendo actualmente esta construida con las tecnologias ya mencionadas.
+                Si quieres saber mas sobre los proyectos en los que he trabajado te invito a visitar la pagina de proyectos
+                y mi repositorio de Github. 
                 {/* Etiam non luctus enim, nec venenatis leo. Proin vitae odio quis lacus molestie rutrum. Aenean vitae lobortis erat. Ut fringilla nisl efficitur lorem blandit, eu finibus metus commodo. Aliquam a velit euismod, aliquet augue at, lacinia dui. Fusce lobortis nisi et velit ultrices vehicula. Aenean a felis nec odio rutrum aliquet. Duis convallis, orci sit amet fermentum vehicula, lorem magna posuere nibh, et molestie lacus eros eget dolor. Maecenas vulputate scelerisque est, eget blandit lacus vestibulum sit amet. Proin ac augue a ipsum venenatis mollis. Morbi scelerisque, est et bibendum sagittis, nisl arcu rutrum ipsum, ut ultricies augue mi vitae lorem. Praesent a sapien viverra, tincidunt erat vel, elementum mi. Donec ex justo, commodo eu leo nec, volutpat faucibus nibh. Aenean quis tristique ante. Maecenas dignissim velit ullamcorper tortor aliquam, vitae dictum odio egestas. Etiam sem ligula, hendrerit nec tortor at, consequat volutpat ipsum. */}
             </p>
             <Link className="link-readMore text-bold show-mobile" to="/project">
