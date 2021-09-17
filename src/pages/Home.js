cee7aeabf0cf638250d81ab8a7e20617d2693710
Franklin Pimentel <franklin.pimentel.1998@gmail.com>
2021-09-16 20:06:28 -0500

    Moving Home styles from assets/styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import './Home.css';
+import React, { useState, useEffect } from 'react';
+import '../assets/styles/Home.css';
 import arrow from '../assets/ArrowreadMore.png';
 import instagram from '../assets/instagram.png';
 import facebook from '../assets/facebook.png';
@@ -8,70 +8,75 @@ import github from '../assets/github.png';
 import global from '../config.js';
 import About from './About';
 
-const Home = (props) => {
+const Home = () => {
     const [animation, setAnimation] = useState("contenido animation-none");
 
+    //Handle animation
     const handleAnimation = () => {
         window.scrollTo(0, 0);
-        setTimeout( () => {
+        setTimeout(() => {
             setAnimation("contenido animation-show");
-        },1);
+        }, 1);
     }
 
-    handleAnimation();
+    useEffect(() => {
+        handleAnimation();
+    }, [])
 
-    return(
+    return (
         <React.Fragment>
-        <section className={animation}>
-            <div className="izq">
-                <aside className="social-media-icons">
-                    <a href={global.instagram}>
-                        <img src={instagram} alt="instagram"/>
-                    </a>
-                    <a href={global.facebook}>
-                        <img src={facebook} alt="facebook"/>
-                    </a>
-                    <a href={global.linkedin}>
-                        <img src={linkedin} alt="linkedin"/>
-                    </a>
-                    <a href={global.github}>
-                        <img src={github} alt="github"/>
-                    </a>
-                </aside>
-                <hr className="vector-about show-mobile"/>
-                <p className="left-info text-ligth"><span className="left-title text-black">Desarrollador <br/> web </span> 
-                    <span style={{color: '#A68E52'}}>autodidacta</span><br/>
-                    en busca de nuevos <br/>
-                    retos.
-                </p>
-                <hr className="vector-about show-mobile"/>
-            </div> 
+            <section className={animation}>
 
-            
-            <div className="der">
-                <hr className="vector-vertical"/>
-                <aside className="language text-ligth">
-                    If you want to navigate in <br/> 
-                    English, change the <br/> 
+                <div className="izq">
+                    <aside className="social-media-icons">
+                        <a href={global.instagram}>
+                            <img src={instagram} alt="instagram" />
+                        </a>
+                        <a href={global.facebook}>
+                            <img src={facebook} alt="facebook" />
+                        </a>
+                        <a href={global.linkedin}>
+                            <img src={linkedin} alt="linkedin" />
+                        </a>
+                        <a href={global.github}>
+                            <img src={github} alt="github" />
+                        </a>
+                    </aside>
+                    <hr className="vector-about show-mobile" />
+                    <p className="left-info text-ligth"><span className="left-title text-black">Desarrollador <br /> web </span>
+                        <span style={{ color: '#A68E52' }}>Front-end</span>
+                        <br />
+                        en busca de nuevos <br />
+                        retos.
+                    </p>
+                    <hr className="vector-about show-mobile" />
+                </div>
+
+
+                <div className="der">
+                    <hr className="vector-vertical" />
+                    <aside className="language text-ligth">
+                        If you want to navigate in <br />
+                    English, change the <br />
                     language
                 </aside>
-                <div className='header'>
-                    <h1 className='header-bold'>Welcome</h1>
-                    <p className='header-ligth'>console.log(“
-                        <span style={{color: '#A68E52', }}> Welcome </span>
+                    <div className='header'>
+                        <h1 className='header-bold'>Franklin Pimentel</h1>
+                        <p className='header-ligth'>console.log(“
+                        <span style={{ color: '#A68E52', }}> React JS </span>
                     ”);</p>
-                </div>
-                <span className='read-more text-ligth'>
-                    Saber 
-                    <span style={{color: '#A68E52', marginRight: '8px'}}>
-                        {' mas'}
+                    </div>
+                    <span className='read-more text-ligth'>
+                        Saber
+                    <span style={{ color: '#A68E52', marginRight: '8px' }}>
+                            {' mas'}
+                        </span>
+                        <img src={arrow} alt="arrow" />
                     </span>
-                    <img src={arrow} alt="arrow" />
-                </span>
-            </div>
-        </section>
-        <About/>
-        </React.Fragment>           
+                </div>
+            </section>
+            <About />
+        </React.Fragment>
     );
 }
 
