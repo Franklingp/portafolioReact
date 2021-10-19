2c65464dcffdd73cf2979131c99e377e83f1522a
Franklin Pimentel <franklin.pimentel.1998@gmail.com>
2021-10-19 12:30:28 -0500

    Se esta revisando el modulo de project card que tiene un errror en version mobile

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,49 +1,9 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
-import './ProjectCard.css';
+import React from 'react'
+import "../assets/styles/ProjectCard.css";
 
-const ProjectCard = (props) => {
-    const { _id, name, date, category, index } = props;
-
-    const handleResponsive = () => {
-        if (window.screen.width > 1000) {
-            if (index % 2 === 0) {
-                return "content-card left-card"
-            } else {
-                return "content-card rigth-card"
-            }
-        } else {
-            console.log("mobile");
-            return "content-card-mobile"
-        }
-    }
-
-    const handleColor = () => {
-        let i = index;
-        if (index > 3) {
-            i = index % 4;
-        }
-        switch (i) {
-            case 0:
-                return ["#A68E52", "#212224"];
-            case 1:
-                return ["#C4C4C4", "#212224"];;
-            case 2:
-                return ["#E3E3E4", "#212224"];
-            default:
-                return ["#212224", "#ffffff"];
-        }
-    }
-
-    const [backgroundColor, color] = handleColor();
-
-    const OnClick = () => {
-        props.history.push(`/project/${_id}`);
-    }
-
+export default function ProjectCard({ name, date, category }) {
     return (
-        <article className={handleResponsive()} style={{ backgroundColor, color }}
-            onClick={OnClick}>
+        <article className={"content-card"}>
             <h2 className="card-title text-regular">{name}</h2>
             <br className="show-mobile" />
             <span className="card-date text-ligth">{date}</span>
@@ -52,5 +12,3 @@ const ProjectCard = (props) => {
         </article>
     )
 }
-
-export default withRouter(ProjectCard);
--- src/pages/ProjectsList.js
+++ src/pages/ProjectsList.js
@@ -8,41 +8,41 @@ import '../assets/styles/loader.css';
 
 const ProjectList = ({ projects }) => {
 
+
+
     //validate project
-    if (projects.length === 0) {
+    if (projects.length < 1) {
         return (
-            <section className={"contenido animate__animated animate__fadeIn"} style={{ backgroundColor: "#212529" }}>
+            <section className={"contenido"} style={{ backgroundColor: "#212529" }}>
                 <div className="loader">Loading...</div>
             </section>
         )
-    }
-
-    return (
-        <section className={"contenido animate__animated animate__fadeIn"}>
-            <div className='main-info'>
-                <h1 className='title-list font-black'>
-                    Portafolio
-                    <br className="show-desktop" />
-                    <span className={`subtitle-list show-desktop animate__animated animate__flipInX`}>Experiencias</span>
-                </h1>
-                <p className="paragraph-list text-ligth">
-                    Esta es una <span>selecion</span> de <br />
-                    algunos de los <span>proyectos</span> en <br />
-                    los que he trabajado<br />
-                    anteriormente.
-                </p>
-            </div>
-            <section className='list-card'>
-                {
-                    projects.map((project, index) => {
-                        return (
-                            <ProjectCard key={project._id} {...project} index={index} />
-                        )
-                    })
-                }
+    } else {
+        return (
+            <section className={"contenido"}>
+                <div className='main-info'>
+                    <h1 className='title-list font-black'>
+                        Portafolio
+                        <br className="show-desktop" />
+                        <span className={`subtitle-list show-desktop`}>Experiencias</span>
+                    </h1>
+                    <p className="paragraph-list text-ligth">
+                        Esta es una <span>selecion</span> de <br />
+                        algunos de los <span>proyectos</span> en <br />
+                        los que he trabajado<br />
+                        anteriormente.
+                    </p>
+                </div>
+                <section className='list-card'>
+                    {
+                        projects.map((project, index) => (
+                            <ProjectCard key={`${project._id}-${index}`} {...project} index={index} />
+                        ))
+                    }
+                </section>
             </section>
-        </section>
-    );
+        );
+    }
 
 }
 
