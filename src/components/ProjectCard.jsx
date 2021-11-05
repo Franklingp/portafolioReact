bcaff13d01b4b3eac630c24413937522dfa74646
Franklin Pimentel <franklin.pimentel.1998@gmail.com>
2021-11-05 17:12:31 -0500

    Los estilos de contacto se han reparado y se ha eliminado bootstrap y material ui antiguo junto con otras dependecias

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -4,7 +4,7 @@ import "../assets/styles/ProjectCard.css";
 const ProjectCard = ({ name, date, category, classess }) => {
 	return (
 		<article className={`content-card ${classess}`}>
-			<h2 className="card-title text-ligth">{name}</h2>
+			<h2 className="card-title text-extra-ligth">{name}</h2>
 			<span className="card-date text-ligth">{date}</span>
 			<span className="card-category text-ligth">{category}</span>
 		</article>
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,141 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { addNewMessage } from "../redux/actions/contactActions";
+import { TextField } from "@mui/material";
+import global from "../config";
+
+//styles
+import "../assets/styles/Contact.css";
+
+const Contact = (props) => {
+	const [form, setForm] = useState({
+		name: "",
+		email: "",
+		message: "",
+	});
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		props.addNewMessage({ ...form });
+		alert(
+			`Gracias ${name} por dejar tu mensaje, proxiamente estare contactando contigo.`
+		);
+		setForm({
+			name: "",
+			email: "",
+			message: "",
+		});
+	};
+
+	const handleChange = (e) => {
+		setForm({
+			...form,
+			[e.target.name]: e.target.value,
+		});
+	};
+
+	return (
+		<section className={"Contact-content animate__animated animate__fadeIn"}>
+			<div className="contact-seccion-izq">
+				<a
+					href={global.instagram}
+					className="big-text"
+					style={{ color: "#A68E52", fontFamily: "Thin-italic" }}
+				>
+					Instagram
+				</a>
+				<a
+					href={global.facebook}
+					className="big-text"
+					style={{ fontFamily: "Extra-ligth" }}
+				>
+					Facebook
+				</a>
+				<a
+					href={global.email}
+					className="big-text"
+					style={{ fontFamily: "Ligth" }}
+				>
+					Correo
+				</a>
+				<a
+					href={global.twitter}
+					className="big-text"
+					style={{ fontFamily: "Regular" }}
+				>
+					Twitter
+				</a>
+				<a
+					href={global.behance}
+					className="big-text"
+					style={{ fontFamily: "Thin-italic" }}
+				>
+					Behance
+				</a>
+				<a
+					href={global.linkedin}
+					className="big-text"
+					style={{ color: "#A68E52", fontFamily: "Extra-ligth" }}
+				>
+					Linkedin
+				</a>
+			</div>
+			<div className="contact-seccion-der">
+				<hr className="vector-contact" />
+				<h2 className="contact-title text-ligth">Contacto</h2>
+
+				<form onSubmit={handleSubmit}>
+					<div className="mb-3">
+						<input
+							type="text"
+							className="custom-input"
+							name="name"
+							placeholder="Nombre"
+							value={form.name}
+							required
+							onChange={handleChange}
+						/>
+					</div>
+
+					<div className="mb-3">
+						<input
+							type="email"
+							className="custom-input"
+							name="email"
+							placeholder="Correo Electronico"
+							value={form.email}
+							onChange={handleChange}
+							required
+						/>
+					</div>
+
+					<div className="mb-3">
+						<textarea
+							type="text"
+							className="custom-input"
+							name="message"
+							placeholder="Mensaje"
+							value={form.message}
+							onChange={handleChange}
+						/>
+					</div>
+
+					<input type="submit" className="btn-contact" value="Enviar" />
+				</form>
+				<p className="text-form text-ligth">
+					Este formulario es con el fin <br />
+					de poder <span className="ligth">contactar</span> contigo
+					posteriormente. <br />
+					No te preocupes tus datos estan{" "}
+					<span className="ligth">seguros.</span>
+				</p>
+			</div>
+		</section>
+	);
+};
+
+const mapDispatchToProps = {
+	addNewMessage,
+};
+
+export default connect(null, mapDispatchToProps)(Contact);
