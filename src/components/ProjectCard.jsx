1c7fe5c2e899bf7aee4094c77ad5815866f3ad0b
Franklin Pimentel <franklin.pimentel.1998@gmail.com>
2021-12-20 17:32:37 -0500

    Utils added in prject, to format text and dates

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "../assets/styles/ProjectCard.css";
 
+//utils
+import { formatData } from "../utils/handleDate";
+import { firtsUpperCase } from "../utils/textFormat";
+
 const ProjectCard = ({ name, date, category, classess }) => {
 	return (
 		<article className={`content-card ${classess}`}>
-			<h2 className="card-title text-extra-ligth">{name}</h2>
-			<span className="card-date text-ligth">{date}</span>
-			<span className="card-category text-ligth">{category}</span>
+			<h2 className="card-title text-extra-ligth">{firtsUpperCase(name)}</h2>
+			<span className="card-date text-ligth">{formatData(date)}</span>
+			<span className="card-category text-ligth">
+				{firtsUpperCase(category)}
+			</span>
 		</article>
 	);
 };
---
+++ src/utils/textFormat.js
@@ -0,0 +1,6 @@
+//Text format utils
+
+//first letter to upercase
+export const firtsUpperCase = (text) => {
+    return text[0].toUpperCase() + text.substring(1);
+}
