bbbef29e21d0012046ce077ca53fe9afd4c59a4c
Franklin Pimentel <franklin.pimentel.1998@gmail.com>
2021-12-20 20:50:20 -0500

    About y Home pasaron de ser .js a .jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import "../assets/styles/Home.css";
+import global from "../config.js";
+import About from "./About";
+
+//Icons
+import arrow from "../assets/ArrowreadMore.png";
+import instagram from "../assets/instagram.png";
+import facebook from "../assets/facebook.png";
+import linkedin from "../assets/linkedin.png";
+import github from "../assets/github.png";
+
+const Home = () => {
+	return (
+		<React.Fragment>
+			<section className="contenido animate__animated animate__fadeIn">
+				<div className="izq">
+					<div />
+					<aside className="social-media-icons">
+						<a href={global.instagram}>
+							<img src={instagram} alt="instagram" />
+						</a>
+						<a href={global.facebook}>
+							<img src={facebook} alt="facebook" />
+						</a>
+						<a href={global.linkedin}>
+							<img src={linkedin} alt="linkedin" />
+						</a>
+						<a href={global.github}>
+							<img src={github} alt="github" />
+						</a>
+					</aside>
+					<hr className="vector-about show-mobile" />
+					<div className="animate__animated animate__fadeInLeft">
+						<p className="left-info text-ligth">
+							<span className="left-title font-black">
+								Desarrollador <br /> web{" "}
+							</span>
+							<span style={{ color: "#A68E52" }}>Front-end</span>
+							<br />
+							en busca de nuevos <br />
+							retos.
+						</p>
+					</div>
+					<hr className="vector-about show-mobile" />
+				</div>
+
+				<div className="der">
+					<hr className="home-custom-vertical-line" />
+					<aside className="language text-ligth">
+						If you want to navigate in <br />
+						English, change the <br />
+						language
+					</aside>
+					<div className="header">
+						<h1 className="header-bold animate__animated animate__fadeInDown">
+							Welcome
+						</h1>
+						<p className="header-ligth">
+							Just do it, in
+							<span style={{ color: "#A68E52" }}> React JS </span>
+						</p>
+					</div>
+					<span className="read-more text-ligth">
+						Saber
+						<span style={{ color: "#A68E52", marginRight: "8px" }}>
+							{" mas"}
+						</span>
+						<img src={arrow} alt="arrow" />
+					</span>
+				</div>
+			</section>
+			<About />
+		</React.Fragment>
+	);
+};
+
+export default Home;
