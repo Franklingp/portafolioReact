1854bdf12a8f0ef4b0f484b6532db98d93f7fec8
Franklin Pimentel <franklin.pimentel.1998@gmail.com>
2022-02-20 16:04:02 -0500

    se removieron los links de social media

--- src/config.js
+++ src/config.js
@@ -2,14 +2,23 @@ const global = {
     //This is the url to connect with the servers api
     url: "https://api-portfolio.herokuapp.com/api",
 
-    //Social media
-    instagram: "https://www.instagram.com/franklinpimentel/",
-    facebook: "https://www.facebook.com/franklin.pimentel/",
-    email: "franklin.pimentel.1998@gmail.com",
-    linkedin: "https://www.linkedin.com/in/franklin-pimentel-a28419194/",
-    github: "https://github.com/Franklingp",
-    behance: "https://www.behance.com/",
-    twitter: "https://www.twitter.com/"
+    // Real Social media
+    // instagram: "https://www.instagram.com/franklinpimentel/",
+    // facebook: "https://www.facebook.com/franklin.pimentel/",
+    // email: "franklin.pimentel.1998@gmail.com",
+    // linkedin: "https://www.linkedin.com/in/franklin-pimentel-a28419194/",
+    // github: "https://github.com/Franklingp",
+    // behance: "https://www.behance.com/",
+    // twitter: "https://www.twitter.com/"
+
+    // test Social media
+    instagram: "/404",
+    facebook: "/404",
+    email: "/404",
+    linkedin: "/404",
+    github: "/404",
+    behance: "/404",
+    twitter: "/404"
 }
 
 export default global;
