fc05b5876e70a3e84d9ba0f7c1a299730a1e1d75
mac <mac@MacBook-Pro.local>
2024-04-29 17:59:34 -0500

    Cleaning auth action

--- src/redux/actions/authActions.js
+++ src/redux/actions/authActions.js
@@ -4,23 +4,12 @@ import { authHttp } from '../../service/fetch';
 //Actions to manage the authentication
 export const logingUserSuccess = createAction("LOGING_USER");
 export const logingUser = data => async (dispatch) => {
-    try {
-        const response = await authHttp("SIGN-IN", data);
-        console.log(response);
-        if (!response.accessToken) {
-            return false;
-        } else {
-            dispatch(logingUserSuccess(response.accessToken));
-            return true;
-        }
-    }
-    catch (error) {
-        console.log(error.response);
-        if (error.response.status === 403 || error.response.status === 404) {
-            return false;
-        } else {
-            throw new Error("Has been a error when trying to login");
-        }
+    const response = await authHttp("SIGN-IN", data);
+    if (!response.accessToken) {
+        return false;
+    } else {
+        dispatch(logingUserSuccess(response.accessToken));
+        return true;
     }
 }
 
